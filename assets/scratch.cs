d974166e300db861aeec957cfb10ffec1e135079
glen03xs <glen03xs@gmail.com>
2021-11-13 20:20:01 -0500

    added user page

---
+++ assets/scratch.cs
@@ -0,0 +1,18 @@
+public class ArithmeticOperations
+    {
+        public decimal Add(decimal n1, decimal n2)
+        {
+            return n1 + n2;
+        }
+
+        public decimal Subract(decimal n1, decimal n2)
+        {
+            return n1 - n2;
+        }
+
+        public decimal Multiply(decimal n1, decimal n2)
+        {
+            return n1 * n2;
+        }
+
+    }
